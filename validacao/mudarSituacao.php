1ad2a9703277d2fa64ab2b20285e896d16be5d49
Jeverson Fagundes <jeverson.2022311922@aluno.iffar.edu.br>
2024-05-26 17:19:45 -0300

    sistema de validação das atividades complementares entregues no sistema

---
+++ validacao/mudarSituacao.php
@@ -0,0 +1,56 @@
+<?php
+
+//conectar com o banco de dados.
+include ("../conecta.php");
+
+if ($_POST['deferir']) {
+    
+    //receber os dados.
+    $natureza = $_POST['natureza'];
+    $id = $_POST['id_atividade'];
+    $cargaDefe = $_POST['cargaDefe'];
+    $situacao = "Deferido";
+    $id_aluno = $_POST['aluno'];
+
+    $sql = "UPDATE entrega_atividade SET natureza = '$natureza', carga_horaria_aprovada = $cargaDefe, status = '$situacao' WHERE id_entrega_atividade = $id";
+
+    mysqli_query($mysql, $sql);
+
+    if ($mysql->error) {
+        
+        die ("Falha ao deferir esta atividade! " . $mysql->error);
+
+    }else {
+        
+        header("location: validar.php?id=".$id_aluno);
+
+    }
+
+}else {
+    
+    if ($_POST['indeferir']) {
+       
+         //receber os dados.
+    $natureza = $_POST['natureza'];
+    $id = $_POST['id_atividade'];
+    $cargaDefe = $_POST['cargaDefe'];
+    $situacao = "Indeferido";
+    $id_aluno = $_POST['aluno'];
+
+    $sql = "UPDATE entrega_atividade SET natureza = '$natureza', carga_horaria_aprovada = $cargaDefe, status = '$situacao' WHERE id_entrega_atividade = $id";
+
+    mysqli_query($mysql, $sql);
+
+    if ($mysql->error) {
+        
+        die ("Falha ao deferir esta atividade! " . $mysql->error);
+
+    }else {
+        
+        header("location: validar.php?id=".$id_aluno);
+
+    }
+
+    }
+}
+?>
---
+++ validacao/validar.php
@@ -0,0 +1,120 @@
+<?php
+
+//receber o id do aluno.
+$id = $_GET['id'];
+
+//conecctar com o banco de dados.
+include ("../conecta.php");
+
+$sql = "SELECT a.id_aluno, a.nome_aluno, a.matricula, a.email,
+
+ ea.id_entrega_atividade, ea.natureza, 
+
+ ea.carga_horaria_certificado, ea.carga_horaria_aprovada, 
+
+ ea.status, ea.certificado, ea.caminho, ea.titulo_certificado
+
+ FROM aluno a
+
+INNER JOIN entrega_atividade ea 
+
+ON a.id_aluno = $id AND ea.id_aluno = $id
+
+";
+
+$resultado = mysqli_query($mysql, $sql);
+
+if ($mysql->error) {
+    
+    die ("Falha ao ver os resultados! " . $mysql->error);
+
+}else {
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Visualização das atividade entregues por um aluno</title>
+
+    <style>
+
+        .card{
+            background-color: white;
+            width: 40%;
+            height: 500px;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px rgba(0, 0, 0, 0.19);
+            margin-bottom: 20px;
+        }
+
+    </style>
+
+</head>
+
+<body>
+
+<nav>
+    <ul>
+        <li><a href="../inicialCoordenador.php">Voltar</a></li>
+    </ul>
+</nav>
+
+<h1>Informações da atividade complementar entregue no sistema para validação</h1>
+
+<?php
+
+while ($dados = mysqli_fetch_assoc($resultado)) {
+    
+    echo '<form action = "mudarSituacao.php" method = "post" >';
+
+    echo '<div class="card">';
+
+    echo '<div class="card-body">';
+
+    echo '<h1 class="card-title">'. 'Titulo do certificado:'. '' .$dados['titulo_certificado'].'</h1>';
+
+    echo '<p class="card-text">'. 'Nome do aluno: '. '' .$dados['nome_aluno'].'</p>';
+
+    echo '<p class="card-text">'. 'Matricula: '. '' .$dados['matricula'].'</p>';
+
+    echo '<p class="card-text">'. 'E-mail: '. '' .$dados['email'].'</p>';
+
+    echo '<select>';
+
+    
+
+    echo '</select>';
+
+    echo '<p class="card-title">'.'O certificado:'. ' ' . '<a href="'. $dados['caminho'].'">'. $dados['certificado'].'</a>'.'</p>';
+
+    echo '<p class="card-text">'. 'Carga horaria do certificado: '. '' .$dados['carga_horaria_certificado'].'</p>';
+
+    echo '<p class="card-text">'. 'Carga horaria deferida: '. '<input type = "number" value = "'.$dados['carga_horaria_aprovada'].'" name = "cargaDefe">'.'</p>';
+
+    echo '<p class="card-text">'. 'Situação:'. ' ' .$dados['status'].'</p>';
+
+    echo '<input type = "hidden" value = "' . $dados['id_entrega_atividade'] .'" name = "id_atividade">';
+
+    echo '<input type = "hidden" value = "' . $dados['id_aluno'] .'" name = "aluno">';
+
+    echo '<input type = "submit" value = "Deferir" name = "deferir">';
+
+    echo '<input type = "submit" value = "Indeferir" name = "indeferir">';
+
+    echo '</div>';
+
+    echo '</div>';
+
+    echo '</form>';
+
+}
+
+?>
+    
+</body>
+
+</html>
