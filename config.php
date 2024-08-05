a4c679a35d03a915afd0007b0c7b29e953bcbbe0
Jeverson Fagundes <jeverson.2022311922@aluno.iffar.edu.br>
2024-08-05 18:55:31 -0300

    blabla

--- config.php
+++ config.php
@@ -6,6 +6,5 @@ $config = [
     'password' => '',
     'banco' => 'jeverson_tcc',
     'email' => 'jeverson.2022311922@aluno.iffar.edu.br',
-    'senha_email' => 'ieiywbhyhegldtdw',
+    'senha_email' => 'xrcflahpxbhzwfoc',
 ];
-
