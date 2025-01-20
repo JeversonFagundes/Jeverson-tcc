7d937646fffa59f2cf7db30014f1c02f64b6a726
Jeverson Fagundes <jeverson.2022311922@aluno.iffar.edu.br>
2025-01-19 21:50:31 -0300

    atualização na escrita do botão

--- crudCoordenador/formeditCoordenador.php
+++ crudCoordenador/formeditCoordenador.php
@@ -128,7 +128,7 @@ $coordenador = mysqli_fetch_assoc($resultado);
                 <div class="row">
                     <div class="col s12">
                         <p class="center-align">
-                            <button class="btn waves-effect waves-light #2e7d32 green darken-3 lighten-3" type="submit" name="action">Cadastrar coordenador de curso
+                            <button class="btn waves-effect waves-light #2e7d32 green darken-3 lighten-3" type="submit" name="action">Alterar coordenador de curso
                                 <i class="material-icons right">send</i> </button>
                         </p>
                     </div>
